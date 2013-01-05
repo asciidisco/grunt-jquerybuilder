45c1784d80b48daa0dbcd9f7867914a803e44dff
Lukas Böcker <me@superlukas.com>
2013-01-05 21:11:24 +0100

    Updated placeholder test

--- test/jquerybuilder_test.js
+++ test/jquerybuilder_test.js
@@ -4,9 +4,10 @@ var grunt = require('grunt');
 // Load local tasks.
 grunt.loadTasks('tasks');
 
-exports['require'] = {
-  setUp: function(done) {
-    // setup here
-    done();
+exports.nodeunit = {
+  please_work: function (test) {
+    test.expect(1);
+    test.ok(true, 'this had better work.');
+    test.done();
   }
 };
